f3366db17b1d258bcd1f5734f4ec5d4d7b366ad5
MalikIbrahim147 <71529842+MalikIbrahim147@users.noreply.github.com>
2020-09-25 20:20:14 +0700

    1.2

---
+++ belajar_laravel/resources/views/master.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="/css/bootstrap.min.css">
+	<title>@yield('title')</title>
+</head>
+<body>
+	@include('_navigasi')
+	@yield('content')
+</body>
+</html>
--- belajar_laravel/routes/web.php
+++ belajar_laravel/routes/web.php
@@ -1,37 +1,14 @@
-<?php
+<?php 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+	return view('home');
 });
 
-//Closure
-Route::get('/profil', function() {
-	return "ini adalah profil siswa";
-})->name('profil');
-
-//Root Name
-Route::get("/tesname", function() {
-	return route('profil');
+Route::get('/about', function() {
+	return view('about');
 });
 
-//Parameter
-Route::get('profil/{id}', function($id) {
-	return $id;
+Route::get('/data', function() {
+	return view('data');
 });
 
-//Controller
-Route::get("/testcontroller", "TestController@show");
-
-//Resorce
-Route::resource('siswa', "SiswaController");
